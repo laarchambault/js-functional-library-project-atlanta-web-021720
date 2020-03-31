83dd1333fdec2a2cf42434dff45135e0a98e51ae
laarchambault <laarchambault@gmail.com>
2020-03-31 14:50:20 -0400

    began lesson

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      for (let i in collection) {
+        callback(collection[i]);
+      }
 
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArr = []
+      for (let i in collection) {
+        newArr = [...newArr, callback(collection[i])];
+      };
+      return newArr;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, [acc]) {
+      for (let i = 0; i < collection.length; i++) {
+        acc = callback(acc, collection[i], collection);
+      }
+      return acc;
+      // for (let i in collection) {
+      //   acc = callback(acc, collection[i], collection);
+      // };
+      // return acc;
     },
 
     functions: function() {
